e414574026417ecea685b4979502d090c483f420
Gessivan Junior <priceddread13@gmail.com>
2023-08-30 22:01:19 -0300

    TELA DE CADASTRO E ICON

---
+++ app/src/main/java/com/example/login/Cadastro.java
@@ -0,0 +1,32 @@
+package com.example.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class Cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+
+        EditText username = (EditText) findViewById(R.id.usernameCadastro);
+
+        MaterialButton regbtn = (MaterialButton) findViewById(R.id.cadastrobtn);
+
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username1 = username.getText().toString();
+                Toast.makeText(Cadastro.this, "O usuário é "+ username1, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
